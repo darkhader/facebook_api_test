dad07d47d3cdbe89dbb2d8638d5a5ba8c67164d3
unknown <dr.hiep98@gmail.com>
2019-08-01 22:30:06 +0700

    first commit

---
+++ facebook-login-node-express-master/configuration/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /*
+   * This file contains the configurations information of Twitter login app.
+   * It consists of Twitter app information, database information.
+   */
+
+  facebook_api_key: "477949126119051",
+  facebook_api_secret: "68f05d463fdcecfcd64bfc23f984925a",
+  callback_url: "https://localhost:3000/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
